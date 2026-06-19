56252879cad817f67776175358c0ea159ccb2f2e
Aisha MHD Al-Dalati <a.dalati4002@gmail.com>
2026-06-19 17:43:31 +0300

    Use Django Redis cache backend

--- ParPro/settings.py
+++ ParPro/settings.py
@@ -132,11 +132,8 @@ REDIS_URL = config("REDIS_URL", default="redis://127.0.0.1:6379/1")
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": REDIS_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
         "KEY_PREFIX": "parpro",
     }
 }
